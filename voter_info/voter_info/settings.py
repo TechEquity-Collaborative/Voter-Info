baa23ef9a08ec1838ee3d0a90bb4048df7f71a79
Eric Sandine <ericsandine@gmail.com>
2018-04-09 19:59:38 -0700

    user workaround

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -138,5 +138,6 @@ else:
         }
     }
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = "{}/libgeos_c.so".format(environ.get('GEOS_LIBRARY_PATH'))
+GDAL_LIBRARY_PATH = "{}/libgdal.so".format(environ.get('GDAL_LIBRARY_PATH'))
+PROJ4_LIBRARY_PATH = "{}/libproj.so".format(environ.get('PROJ4_LIBRARY_PATH'))
