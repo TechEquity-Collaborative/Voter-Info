c2a472225b41ad1d776b835ffeb6912e94fed3b7
Eric Sandine <ericsandine@gmail.com>
2018-04-14 14:32:12 -0700

    user the right directory

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
+REACT_APP_DIR = os.path.join(BASE_DIR, 'voter_info.frontend')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
