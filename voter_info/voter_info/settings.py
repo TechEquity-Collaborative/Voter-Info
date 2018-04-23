bf8232fbf0d0bc911a854f9135e7efe80aa95f49
Eric Sandine <ericsandine@gmail.com>
2018-04-23 19:43:31 -0400

    Frontend Dev and Prod (#26)
    * app.json changes and wsgi
    
    * config vars must be strings
    
    * ok this duplicate naming is not working
    
    * fix crappy find and replace
    
    * fine we'll cd in
    
    * revert a bunch of changes
    
    * more cleanup
    
    * more cleanup
    
    * readme update and rm unneeded __init__
    
    * init front end
    
    * eject from create react app
    
    * add routes for dev and prod
    
    * react build
    
    * user the right directory
    
    * directory structure is key here
    
    * add front end build settings
    
    * fix local procfile
    
    * restructure to get heroku to build for us
    
    * fix node buildpack name
    
    * node will inject it's bundles into djangos static dir
    
    * heroku seems happier with yarn
    
    * Updated Yarn lockfile
    
    * add some more to the readme
    
    * fix static directory
    
    * fix package.json
    
    * fix webpack config
    
    * fix static file locations
    
    * update yarn instructions
    
    * Revert "update yarn instructions"
    
    This reverts commit e4b98fee9d3cc7e2569b614b11d97db84e72b008.
    
    * update readme for yarn
    
    * readme update

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -55,6 +55,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+if not IN_PRODUCTION:
+    MIDDLEWARE.append('voter_info.middleware.dev_cors_middleware')
+
 ROOT_URLCONF = 'voter_info.urls'
 
 TEMPLATES = [
@@ -113,9 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-
 
 if IN_PRODUCTION:
     # Configure Django App for Heroku.
