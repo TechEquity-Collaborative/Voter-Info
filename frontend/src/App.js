64fa21162b0d1e715f6ab45fd310c6ebefc2dd89
Eric Sandine <ericsandine@gmail.com>
2018-05-09 23:09:37 -0700

    basic front end with search -> scroll to results

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,63 @@
-import React, { Component } from 'react';
-import SearchBar from './SearchBar';
+import React, {Component} from 'react';
+import SearchBar from './SearchBar'
+import Result from './Result'
+import {Jumbotron, Row, Col, Grid} from 'react-bootstrap'
 import './App.css';
+import scrollToComponent from 'react-scroll-to-component';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      address: null
+    }
+  }
+
+  getResults = (address, lat, long) => {
+    this.setState({
+      address,
+      results: ['hey', 'sup', 'yo', lat, long]
+    }, () => scrollToComponent(this.resultsRow, {align: 'top'}))
+    console.log(address, lat, long)
+  }
+
   render() {
     return (
-      <div className="App">
-        <SearchBar />
-      </div>
-    );
+      <React.Fragment>
+        <Jumbotron className="jumbo-style">
+          <Row style={{
+            marginTop: '18%'
+          }}>
+            <Col md={6} mdOffset={3} sm={8} smOffset={2} xs={8} xsOffset={2}>
+              <h2 style={{color: 'white'}}>
+                FIND WHO REPRESENTS ME
+              </h2>
+              <p style={{color: 'white'}}>
+                Get in touch with the elected officials that serve you. Enter your address below
+                to find out who represents you.
+              </p>
+              <SearchBar onSelect={this.getResults}/>
+            </Col>
+          </Row>
+        </Jumbotron>
+        <Grid ref={(e) => this.resultsRow = e}>
+          {this.state.address && this.state.results.length > 0 && <Row>
+            <Col md={8} mdOffset={2}>
+              <h1>My Representatives</h1>
+              <p>These are the officials that serve {this.state.address}</p>
+              {this
+                .state
+                .results
+                .map((r, i) => {
+                  return <Result key={i} data={r}/>
+                })}
+            </Col>
+          </Row>
+}
+        </Grid>
+      </React.Fragment>
+    )
   }
 }
 
