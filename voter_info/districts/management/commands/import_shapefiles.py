d5d2cf769d49b07bbb7b36f10596ebb7531a0983
Ben Mathes <mathes.ben@gmail.com>
2018-05-09 22:47:18 -0700

    wip to be squashed

--- voter_info/districts/management/commands/import_shapefiles.py
+++ voter_info/districts/management/commands/import_shapefiles.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from django.core.management import BaseCommand
 from django.contrib.gis.gdal import DataSource
@@ -7,34 +8,41 @@ from django.contrib.gis.utils import LayerMapping
 import districts
 from districts.models import District
 
-WORLD_APP_DATA_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(districts.__file__)))
+DISTRICTS_APP_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(districts.__file__)))
 
-SHAPE_FILE_NAME = 'geo_export_efa8ecd8-3d0c-424b-bf93-d52b75f0e330.shp'
+SHAPE_FILE_NAME = 'geo_export_d7286284-cf00-4f7f-bb87-739e415712fe.shp'# 'geo_export_efa8ecd8-3d0c-424b-bf93-d52b75f0e330.shp'
 
 django_model_to_shapefile_key = {
-    'shape_stle': 'shape_stle',
-    'dist_name': 'dist_name',
-    'shape_star': 'shape_star',
-    'district_i': 'district_i',
     'mpoly' : 'MULTIPOLYGON',
 }
 
 
+# 1: one shapefile package per district (1-1 to office).
+# 2: one shapefile may have many areas, which belong to one District. (so districts.models will have 2 models)
+# 3: sub-directory each shapefile package.
+# 4: try to ignore the "schema" of the shapefiles. just a name will do.
+
 class Command(BaseCommand):
-    help = "imports all shapefiles in world/data/"
+    help = "idempotently imports all shapefiles in voter_info/districts/shapefiles/"
 
     def handle(self, *args, **options):
-        shape_file_path = f'{WORLD_APP_DATA_DIRECTORY}/shape_files/{SHAPE_FILE_NAME}'
-        data_source = DataSource(shape_file_path)
-        # TODO(benmathes): this layer mapping is failing. The django_model_to_shapefile_key
-        # is from the geoDjango tutorial, which comes with its own shapefile. Perhaps the
-        # mapping is peculiar to each shapefile?
-        # https://docs.djangoproject.com/en/2.0/ref/contrib/gis/layermapping/
-        layer_mapping = LayerMapping(
-            District,
-            shape_file_path,
-            django_model_to_shapefile_key,
-            transform=False,
-            encoding='iso-8859-1',
-        )
-        layer_mapping.save(strict=True, verbose=True)
+        shape_file_path = {SHAPE_FILE_NAME}
+
+
+        pathlist = Path(f'{DISTRICTS_APP_DIRECTORY}/shape_files/').glob('**/*.shp')
+        import pdb; pdb.set_trace()
+        for shape_file_path in pathlist:
+            data_source = DataSource('/'.join(shape_file_path.parts))
+
+            # TODO(benmathes): this layer mapping is failing. The django_model_to_shapefile_key
+            # is from the geoDjango tutorial, which comes with its own shapefile. Perhaps the
+            # mapping is peculiar to each shapefile?
+            # https://docs.djangoproject.com/en/2.0/ref/contrib/gis/layermapping/
+            layer_mapping = LayerMapping(
+                Area,
+                shape_file_path,
+                django_model_to_shapefile_key,
+                transform=False,
+                encoding='iso-8859-1',
+            )
+            layer_mapping.save(strict=True, verbose=True)
--- voter_info/districts/models.py
+++ voter_info/districts/models.py
@@ -1,44 +1,61 @@
+
+import django
+from django.core.serializers.json import DjangoJSONEncoder
+from django.contrib.postgres.fields import JSONField
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import GEOSGeometry
 
 
+class District(models.Model):
+    """
+    A district that's electing an office. Can have many areas,
+    as specified by our 3rd-party shapefiles.
+    """
+    name = models.TextField()
+
+    shape_file_name = models.TextField()
+
+
+
 # a custom queryset and manager to be able to write:
-# District.objects.contains_point(lat, lon) -> QuerySet[ <District 1>, <District 2>,... ]
-class DistrictQuerySet(models.QuerySet):
+# Area.objects.contains_point(lat, lon) -> QuerySet[ <Area 1>, <Area 2>,... ]
+class AreaQuerySet(models.QuerySet):
     def contains_point(self, lat, lon):
         return self.filter(mpoly__contains=GEOSGeometry(f'POINT({lat} {lon})'))
 
 
-class DistrictManager(models.Manager):
+class AreaManager(models.Manager):
     def get_queryset(self):
-        return DistrictQuerySet(self.model, using=self._db)
+        return AreaQuerySet(self.model, using=self._db)
 
     def contains_point(self, lat, lon):
         return self.get_queryset().contains_point(lat, lon)
 
 
-class District(models.Model):
+class Area(models.Model):
+    """
+    A geographic area. The external shape files we are using
+    have many areas within an office's district can have many areas, e.g.
+    a city-wide compaign can
+    """
+    objects = AreaManager()
 
-    objects = DistrictManager()
+    district = models.ForeignKey(District, on_delete=django.db.models.deletion.CASCADE, related_name='areas')
 
-    # Regular Django fields corresponding to the attributes in the
-    # world borders shapefile.
     name = models.CharField(max_length=50)
 
+    # a json field corresponding to the attributes in the shapefile.
+    # each shapefile can have a different schema.
     # you can find these fields with the `ogrinfo` shell utility
     # installed with the GIS libraries.
     # https://docs.djangoproject.com/en/2.0/ref/contrib/gis/tutorial/#use-ogrinfo-to-examine-spatial-data
-    shape_stle = models.FloatField('shape style')
-    dist_name = models.TextField('district name')
-    shape_star = models.FloatField('shape star')
-    district_i = models.FloatField('district ID')
+    # or programatically with
+    extra_fields = JSONField(encoder=DjangoJSONEncoder, null=True)
 
     # GeoDjango-specific: a geometry field (MultiPolygonField)
     mpoly = models.MultiPolygonField()
 
-    # Returns the string representation of the model.
-    def __str__(self):
-        return self.name
+
 
 
 
