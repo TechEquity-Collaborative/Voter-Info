8c63245e34372f4eeb2309f109458e7230aa818e
Ben Mathes <mathes.ben@gmail.com>
2018-08-22 14:49:12 -0700

    admin ui cleanup

--- voter_info/districts/admin.py
+++ voter_info/districts/admin.py
@@ -11,12 +11,13 @@ def ingest_shapefile_from_csv(model_admin, request, queryset):
     this function is run when you're in the District admin UI
     and run "ingest shapefiles from CSV"
     '''
-    IngestShapefilesCommand.download_shapefiles_from_urls_in_csv()
-    IngestShapefilesCommand.extract_shapefiles_into_database()
+    ingester = IngestShapefilesCommand()
+    ingester.download_shapefiles_from_urls_in_csv()
+    ingester.extract_shapefiles_into_database()
 
 
 # docs here: docs.djangoproject.com/en/1.11/ref/contrib/admin/actions/#adding-actions-to-the-modeladmin
-ingest_shapefile_from_csv.short_description = 'upserts the offices and districts from the CSV with district/office names and links to the shapefiles'
+ingest_shapefile_from_csv.short_description = 'ingest offices and districts. Takes several minutes. Ok to run multiple times.'
 
 
 class DistrictAdmin(admin.ModelAdmin):
--- voter_info/offices/models.py
+++ voter_info/offices/models.py
@@ -6,17 +6,18 @@ from districts.models import District
 class Office(models.Model):
     class Meta:
         unique_together = (('name', 'district'),)
+        ordering = ('district', 'name')
 
     name = models.TextField(null=False)
     district = models.ForeignKey(District, on_delete=models.CASCADE, null=False, related_name='offices')
 
     def __str__(self):
-        return f'<Office id={self.id} name="{self.name}" district="{self.district.name}" district_id={self.district.id}>'
-
+        return f'<Office id={self.id} district="{self.district.name}" name="{self.name}" district_id={self.district.id}>'
 
 
 class Candidate(models.Model):
     class Meta:
+        ordering = ('office__district', 'office', 'name')
         unique_together = (
             ('name', 'office'),
         )
@@ -27,4 +28,4 @@ class Candidate(models.Model):
     office = models.ForeignKey(Office, on_delete=models.CASCADE, null=False, related_name='candidates')
 
     def __str__(self):
-        return f'<Candidate id={self.id} name="{self.name}" incumbent={self.incumbent} office="{self.office.name}" office_id={self.office.id} office_district="{self.office.district.name}">'
+        return f'<Candidate id={self.id} district="{self.office.district.name}"  office="{self.office.name}" name="{self.name}" incumbent={self.incumbent} office_id={self.office.id}>'
