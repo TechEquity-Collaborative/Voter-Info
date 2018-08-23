a731f55b5c50f573fbb8f27574a951a8576cbab3
Eric Sandine <ericsandine@gmail.com>
2018-08-23 16:44:32 -0400

    normalize path

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 
 if IN_PRODUCTION:
