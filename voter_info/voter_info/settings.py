03e3850e249393f37a25f4ea0fd4ffce59c00023
Eric Sandine <ericsandine@gmail.com>
2018-09-09 15:50:52 -0700

    rm STATICFILES_DIRS

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -131,9 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if IN_PRODUCTION:
