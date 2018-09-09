99be00d977af80fda9e52a99db82af81da91f482
Eric Sandine <ericsandine@gmail.com>
2018-09-09 16:56:01 -0700

    update static routes

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 # Django static files
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
