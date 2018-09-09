56c75f50d9f532dada4f7a2ae82d0c0feba8c5e9
Eric Sandine <ericsandine@gmail.com>
2018-09-09 16:34:14 -0700

    add react directory

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -132,7 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# Django static files
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# React built files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "staticfiles"),
+)
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
