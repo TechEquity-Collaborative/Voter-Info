adb7eea63dad8babbd3a322888223ae2774e5719
Eric Sandine <ericsandine@gmail.com>
2018-09-09 16:24:04 -0700

    user proeject root

--- voter_info/voter_info/settings.py
+++ voter_info/voter_info/settings.py
@@ -15,6 +15,7 @@ import raven
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 env = os.environ.copy()
 IN_PRODUCTION = os.getenv('ON_HEROKU', False)
@@ -131,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
